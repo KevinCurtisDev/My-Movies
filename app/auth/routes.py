6b570b402aa472d7b07c6913d07c18a9f9de487b
KevinCurtisDev <kpcurtis@gmail.com>
2019-07-30 21:37:20 +0100

    Completed login/ sign up/ log out logic

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, login_required,  current_user
 from app.auth.forms import RegistrationForm
+from app.auth.forms import LoginForm
 from app.auth import authentication as at
 from app.auth.models import User
 
@@ -21,15 +22,22 @@ def register_user():
 
 @at.route('/login', methods=['GET', 'POST'])
 def signin_user():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
 
-    form = LoginForm()
-    if form.validate_submit():
-        user = User.query.filter_by(user_email=form.email.data).first()
-        if not user or not user.check_password(form.password.data):
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(user_email=login_form.email.data).first()
+        if not user or not user.check_password(login_form.password.data):
             flash('Invalid login, try again')
             return redirect(url_for('authentication.signin_user'))
 
-        login_user(user, form.stay_loggedin.data)
-        return redirect(url_for('main.show_movies'))
-        
-    return render_template('login.html')
+        login_user(user, login_form.stay_loggedin.data)
+        return redirect(url_for('main.index'))
+    return render_template('login.html', login_form=login_form)
+
+@at.route('/logout')
+@login_required
+def log_out_user():
+    logout_user()
+    return redirect(url_for('main.index'))
--- app/movies/routes.py
+++ app/movies/routes.py
@@ -7,10 +7,6 @@ from flask import render_template, url_for, request
 def index():
     return render_template('index.html')
 
-@main.route('/login')
-def login():
-    return render_template('login.html')
-
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('notfound.html'), 404
